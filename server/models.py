5e85da3c37fbe236f270a88bed00e90711e3e437
AyimWilliam <odhisayim@gmail.com>
2023-08-01 09:33:45 +0300

    finished updating Joshua's models

--- server/models.py
+++ server/models.py
@@ -21,7 +21,7 @@ class User(db.Model, SerializerMixin):
     serialize_rules = ("-donation.user")
 
     def __repr__(self):
-        return f'User: {self.user_name}, ID: {self.id}'
+        return f'User: {self.user_name}, ID: {self.id}, Role: {self.role}'
 
 class Charity(db.Model, SerializerMixin):
     __tablename__ = 'charities'
@@ -38,6 +38,9 @@ class Charity(db.Model, SerializerMixin):
 
     serialize_rules = ("-beneficiaries.charity", "-donations.charity", "-inventory.charity")
 
+    def __repr__(self):
+        return f'Name: {self.name} of ID: {self.charity_id} has received {self.amount_received}'
+
 class Donation(db.Model, SerializerMixin):
     __tablename__ = 'donations'
 
@@ -53,26 +56,40 @@ class Donation(db.Model, SerializerMixin):
     schedule_end_date = db.Column(db.DateTime)
     schedule_frequency = db.Column(db.String)
 
-    # donor = relationship('User', back_populates='donations')
-    # charity = relationship('Charity', back_populates='donations')
+    serialize_rules = ("-users.donations", "-charities.donations")
+
+
+    def __repr__(self):
+        return f"{self.amount} was donated on {self.donation_date} by donor of ID: {self.donor_id}"
 
 class Beneficiary(db.Model, SerializerMixin):
     __tablename__ = 'beneficiaries'
 
-    beneficiary_id = Column(Integer, primary_key=True)
-    charity_id = Column(Integer, ForeignKey('charities.charity_id'), nullable=False)
-    beneficiary_name = Column(String, nullable=False)
-    story = Column(String)
+    beneficiary_id = db.Column(db.Integer, primary_key=True)
+    charity_id = db.Column(db.Integer, db.ForeignKey('charities.charity_id'), nullable=False)
+    beneficiary_name = db.Column(db.String, nullable=False)
+    story = db.Column(db.String)
 
-    charity = relationship('Charity', back_populates='beneficiaries')
+    serialize_rules = ("-charity.beneficiaries")
 
-class Inventory(Base):
+    def __repr__(self):
+        return f"{self.beneficiary_name} of charity ID:{self.charity_id} says {self.story}"
+    
+
+    
+
+class Inventory(db.Model, SerializerMixin):
     __tablename__ = 'inventory'
 
-    inventory_id = Column(Integer, primary_key=True)
-    charity_id = Column(Integer, ForeignKey('charities.charity_id'), nullable=False)
-    item_name = Column(String, nullable=False)
-    quantity = Column(Integer, nullable=False)
-    date_sent = Column(DateTime, nullable=False)
+    inventory_id = db.Column(db.Integer, primary_key=True)
+    charity_id = db.Column(db.Integer, db.ForeignKey('charities.charity_id'), nullable=False)
+    item_name = db.Column(db.String, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    date_sent = db.Column(db.DateTime, nullable=False)
 
-    charity = relationship('Charity', back_populates='inventory_items')
+    serialize_rules = ("-charity.inventory")
+
+    def __repr__(self):
+        return f"{self.item_name} was sent on {self.date_sent} by charity{self.charity_id}"
+
+    
