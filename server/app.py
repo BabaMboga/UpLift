13b221ea27da1e47dbbd5e75592cbb18fff40fd0
AyimWilliam <odhisayim@gmail.com>
2023-08-01 14:22:42 +0300

    Finished validation in sign up

--- server/app.py
+++ server/app.py
@@ -1,25 +1,40 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 from werkzeug.security import generate_password_hash
+from flask_migrate import Migrate
+from flask_cors import CORS
+from models import db, User, Charity, Donation, Beneficiary, Inventory
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your-secret-key')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///uplift.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'your-jwt-secret-key'  # Change this to a strong secret key
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+
 jwt = JWTManager(app)
+CORS(app)
+migrate = Migrate(app,db)
+db.init_app(app)
 
 # Manually create the application context
 # with app.app_context():
     # Create the database tables (Since we removed User table, we don't need this anymore)
     # db.create_all()
 
-@app.route('/signup', methods=['POST'])@jwt_required  # Require a valid JWT token to access this endpoint
+@app.route('/')
+def index():
+    return "This is The UpLift User/Charit/Donation/Beneficiary/Inventory API"
+
+@app.route('/signup', methods=['POST'])  # Require a valid JWT token to access this endpoint
 def signup():
     data = request.get_json()
     if not data:
         return jsonify({'message': 'Missing JSON data in the request'}), 400
 
+    # Extract data from the JSON request
     role = data.get('role')
     username = data.get('username')
     first_name = data.get('first_name')
@@ -27,13 +42,39 @@ def signup():
     email = data.get('email')
     password = data.get('password')
 
+    #Validate required fields
     if not role or not username or not first_name or not last_name or not email or not password:
         return jsonify({'message': 'Missing required fields'}), 400
 
+    #Validate role
     if role not in ('user', 'admin', 'charity'):
         return jsonify({'message': 'Invalid role'}), 400
+    
+    #Validate email format
+    if '@' not in email or '.' not in email:
+        return jsonify({'message':'Invalid email format'}), 400
+    
+    #Check if the email is already registered
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user:
+        return jsonify({'message': 'Email is already registered'}), 400
+    
+    #Generate a password hash
+    hashed_password = generate_password_hash(password)
 
-    # Save the user information in the database (code to save data to the database goes here)
+    #Create a new User object
+    new_user = User(
+        email=email,
+        password=hashed_password,
+        role=role,
+        user_name=username,
+        first_name=first_name,
+        second_name=last_name
+    )
+
+    # Save the user information in the database
+    db.session.add(new_user)
+    db.session.comit()
 
     return jsonify({'message': 'User created successfully'}), 201
 
