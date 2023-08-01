4ee36775ba7332b7006c1f33f27f4ab74b323e5f
AyimWilliam <odhisayim@gmail.com>
2023-08-01 10:27:06 +0300

    Successfully merged Ron work

--- server/app.py
+++ server/app.py
@@ -37,5 +37,24 @@ def signup():
 
     return jsonify({'message': 'User created successfully'}), 201
 
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    if not data:
+        return jsonify({'message': 'Missing JSON data in the request'}), 400
+
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({'message': 'Missing username or password'}), 400
+
+    # Validate the user's credentials and retrieve the user from the database (code for validation and retrieval goes here)
+
+    # For the sake of this example, we'll create a dummy access token
+    access_token = 'dummy-access-token'
+
+    return jsonify({'access_token': access_token}), 200
+
 if __name__ == '__main__':
     app.run(debug=True)
