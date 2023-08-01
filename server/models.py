c76d71ab5e180d9a4dd3f1cd24b94bb746e4cc27
AyimWilliam <odhisayim@gmail.com>
2023-08-01 06:44:57 +0300

    updated models not finished

--- server/models.py
+++ server/models.py
@@ -1,51 +1,62 @@
-from sqlalchemy import Column, Integer, String, Boolean, Float, DateTime, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.sql import func
 
-Base = declarative_base()
 
-class User(Base):
+db = SQLAlchemy()
+
+class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
     
-    id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    role = Column(String, nullable=False)
-    user_name = Column(String, nullable=False)
-    first_name = Column(String, nullable=False)
-    second_name = Column(String, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, nullable=False, unique = True)
+    password = db.Column(db.String, nullable = False, )
+    role = db.Column(db.String, nullable=False)
+    user_name = db.Column(db.String, nullable=False)
+    first_name = db.Column(db.String, nullable=False)
+    second_name = db.Column(db.String, nullable=False)
 
-    donations = relationship('Donation', back_populates='donor')
+    donations = db.relationship('Donation', backref='donor')
 
-class Charity(Base):
+    serialize_rules = ("-donation.user")
+
+    def __repr__(self):
+        return f'User: {self.user_name}, ID: {self.id}'
+
+class Charity(db.Model, SerializerMixin):
     __tablename__ = 'charities'
 
-    charity_id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    description = Column(String, nullable=False)
-    status = Column(Boolean, nullable=False)
-    amount_received = Column(Integer)
+    charity_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    status = db.Column(db.Boolean, nullable=False)
+    amount_received = db.Column(db.Integer)
 
-    beneficiaries = relationship('Beneficiary', back_populates='charity')
-    donations = relationship('Donation', back_populates='charity')
-    inventory_items = relationship('Inventory', back_populates='charity')
+    beneficiaries = db.relationship('Beneficiary', backref='charity')
+    donations = db.relationship('Donation', backref='charity')
+    inventory = db.relationship('Inventory', backref='charity')
 
-class Donation(Base):
+    serialize_rules = ("-beneficiaries.charity", "-donations.charity", "-inventory.charity")
+
+class Donation(db.Model, SerializerMixin):
     __tablename__ = 'donations'
 
-    donation_id = Column(Integer, primary_key=True)
-    donor_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    charity_id = Column(Integer, ForeignKey('charities.charity_id'), nullable=False)
-    amount = Column(Float, nullable=False)
-    donation_date = Column(DateTime, nullable=False)
-    is_anonymous = Column(Boolean, nullable=False)
-    schedule_start_date = Column(DateTime)
-    schedule_end_date = Column(DateTime)
-    schedule_frequency = Column(String)
+    donation_id = db.Column(db.Integer, primary_key=True)
+    donor_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    charity_id = db.Column(db.Integer, db.ForeignKey('charities.charity_id'), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    donation_date = db.Column(db.DateTime, default=func.now())
+    is_anonymous = db.Column(db.Boolean, nullable=False)
 
-    donor = relationship('User', back_populates='donations')
-    charity = relationship('Charity', back_populates='donations')
+    #Donation schedule fields
+    schedule_start_date = db.Column(db.DateTime)
+    schedule_end_date = db.Column(db.DateTime)
+    schedule_frequency = db.Column(db.String)
 
-class Beneficiary(Base):
+    # donor = relationship('User', back_populates='donations')
+    # charity = relationship('Charity', back_populates='donations')
+
+class Beneficiary(db.Model, SerializerMixin):
     __tablename__ = 'beneficiaries'
 
     beneficiary_id = Column(Integer, primary_key=True)
